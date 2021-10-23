b66eb79fb4df85dfbb21ba6d9fd83e2c50c2ef9b
Vishal Devireddy <TwoTau@users.noreply.github.com>
2021-10-23 14:35:26 -0700

    Add starter code for express app

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+});
